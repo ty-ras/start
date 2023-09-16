9644e86e35e94b061756feb1f73647f63d4fc608
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-09-16 20:10:44 +0300

    #36 No need to rollup .d.ts files in tests.

--- code/rollup.config.ts
+++ code/rollup.config.ts
@@ -7,58 +7,65 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import license from "rollup-plugin-license";
 
 const outDir = "bundle";
-const config = defineConfig({
-  input: await globby("dist/*.mjs"),
-  output: {
-    dir: outDir,
-    interop: "esModule",
-    generatedCode: {
-      preset: "es2015",
-    },
-    chunkFileNames: "[name].mjs",
-    entryFileNames: "[name].mjs",
-    manualChunks: (id) => {
-      if (id.includes("node_modules")) {
-        return "dependencies";
-      }
-    },
-    hoistTransitiveImports: false,
-  },
-  treeshake: {
-    moduleSideEffects: "no-external",
-  },
-  plugins: [
-    nodeResolve({
-      browser: false,
-      // Without this, the chalk will fail:
-      // https://github.com/chalk/supports-color/issues/113
-      exportConditions: ["node"],
-    }),
-    commonjs({
-      include: "node_modules/**",
-    }),
-    json(),
-    license({
-      thirdParty: {
-        output: `${outDir}/licenses.md`,
+
+const rollupConfigs = [
+  defineConfig({
+    input: await globby("dist/*.mjs"),
+    output: {
+      dir: outDir,
+      interop: "esModule",
+      generatedCode: {
+        preset: "es2015",
       },
-    }),
-  ],
-});
+      chunkFileNames: "[name].mjs",
+      entryFileNames: "[name].mjs",
+      manualChunks: (id) => {
+        if (id.includes("node_modules")) {
+          return "dependencies";
+        }
+      },
+      hoistTransitiveImports: false,
+    },
+    treeshake: {
+      moduleSideEffects: "no-external",
+    },
+    plugins: [
+      nodeResolve({
+        browser: false,
+        // Without this, the chalk will fail:
+        // https://github.com/chalk/supports-color/issues/113
+        exportConditions: ["node"],
+      }),
+      commonjs({
+        include: "node_modules/**",
+      }),
+      json(),
+      license({
+        thirdParty: {
+          output: `${outDir}/licenses.md`,
+        },
+      }),
+    ],
+  }),
+];
 
-// At least for now, this is close to useless, as index.d.mts is already not referencing anything external
-// But maybe in the future it will be more complex, actually exporting something related to this library.
-const dtsConfig = defineConfig({
-  input: "dist-ts/index.d.mts",
-  output: {
-    file: `${outDir}/index.d.mts`,
-    format: "es",
-  },
-  plugins: [
-    dts({
-      respectExternal: true,
+if (process.env["TYRAS_START_ONLY_TEST"] !== "true") {
+  // At least for now, this is close to useless, as index.d.mts is already not referencing anything external
+  // But maybe in the future it will be more complex, actually exporting something related to this library.
+  rollupConfigs.push(
+    defineConfig({
+      input: "dist-ts/index.d.mts",
+      output: {
+        file: `${outDir}/index.d.mts`,
+        format: "es",
+      },
+      plugins: [
+        dts({
+          respectExternal: true,
+        }),
+      ],
     }),
-  ],
-});
+  );
+}
 
-export default [config, dtsConfig];
+export default rollupConfigs;
